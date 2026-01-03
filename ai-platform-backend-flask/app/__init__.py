6eafb5a725fd114c9b270a8584650400e3f7d472
ilhamAmgoun200 <165905850+ilhamAmgoun200@users.noreply.github.com>
2026-01-03 14:07:18 +0100

    Update CORS origins in Flask app configuration

--- ai-platform-backend-flask/app/__init__.py
+++ ai-platform-backend-flask/app/__init__.py
@@ -32,7 +32,7 @@ def create_app(config_class=Config):
         resources={r"/*": {  # Permet TOUS les endpoints
             "origins": ["http://localhost:3000",
                         "http://localhost:3001",
-                        "https://ai-educative-1344.onrender.com"],
+                        "https://ai-educative-1344-5g6z.onrender.com"],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"],
             "expose_headers": ["Content-Type", "Authorization"],
